52ee3298a1bf342688cb35b2fdd48931b4bf3aeb
botisall <76204264+botisall@users.noreply.github.com>
2022-06-29 22:49:21 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0896-3026-7618 DANA]
+│ •  [0895-3230-40460 DANA]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
