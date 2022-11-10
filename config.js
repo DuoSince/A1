cb5866071ab3beac90224429bd8a700443905a60
DuoSince <96527050+DuoSince@users.noreply.github.com>
2022-11-10 22:08:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6289630267618'],
-  ['6285693402116'],
-  ['62895323040460', 'Owner', true]
+  ['6285609524655'],
+  ['6285609524655'],
+  ['6285609524655', 'DevXyZ', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@Queenzy'
+  var sticker_author = 'DevXyZ'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© DevXyZ'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
