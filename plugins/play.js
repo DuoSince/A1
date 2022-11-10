e26ad31859f59d2ca50efb3c32165606d5ca913c
DuoSince <96527050+DuoSince@users.noreply.github.com>
2022-11-10 23:07:15 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,63 +1,23 @@
-let limit = 30
-let yts = require('yt-search')
 let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
-  if (!text) throw `What song are you want find?\n*Example:* ${usedPrefix}play alan walker faded`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Not Found'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry another server...'}`)
-    }
-  }
-  if (yt === false) throw 'All servers can\'t 😕'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
-*Server y2mate:* ${usedServer}
+let handler = async(m, { conn, text }) => {
+    if (!text) throw 'masukin judulnya!'
+    let yt = await fetch(`https://zenzapis.xyz/downloader/y2mate?apikey=SyaizZ&query=${text}`)
+    let json = await yt.json()
+    if (!json.status) throw json
+    let js = json.result
+    let txt = `
+[ *PLAY MUSIC* ]
+*title:* ${js.title}
+*size:* ${js.sizeAudio}
+link ${js.link}
 
-_*Please wait while processing..*_
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+_*sabar audio akan dikirim*_
+`
+    conn.sendFile(m.chat, js.thumb, '', txt, m)
+    conn.sendFile(m.chat, json.result.getAudio, '', '', m)
 }
-handler.help = ['play', 'play2'].map(v => v + ' <search>')
+handler.help = ['play']
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
-handler.premium = false
-handler.group = true
-
-handler.exp = 0
-handler.limit = true
+handler.command = /^(ytp|play)$/i
 
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
